a4ac98303d77db723d0867b31b25800ccde889b9
Arnar Gauti Ingason <arnarg@fastmail.com>
2019-03-25 23:14:18 +0000

    Adding support for both 1 and 2 GiB variants

--- Makefile
+++ Makefile
@@ -2,6 +2,8 @@ UBOOT_DEFCONFIG ?= p281_defconfig
 UBOOT_DIR ?= u-boot
 UBOOT_TAG ?= v2019.01-tx3-mini
 
+RAM_SIZE ?= 2g
+
 TOOLCHAIN ?= aarch64-linux-gnu-
 
 UBOOT_OUTPUT_DIR ?= $(TMP_DIR)/u-boot
@@ -12,10 +14,10 @@ FIP_DIR ?= $(UBOOT_OUTPUT_DIR)/fip
 UBOOT_MAKE ?= make -C $(UBOOT_DIR) KBUILD_OUTPUT=$(UBOOT_OUTPUT_DIR) ARCH=arm CROSS_COMPILE="$(TOOLCHAIN)"
 
 .PHONY: all
-all: $(OUT_DIR)/u-boot-$(UBOOT_TAG).bin
+all: $(OUT_DIR)/u-boot-$(UBOOT_TAG)-$(RAM_SIZE).bin
 
-$(OUT_DIR)/u-boot-$(UBOOT_TAG).bin: $(OUT_DIR) $(FIP_DIR)/u-boot.bin.sd.bin
-	cp $(FIP_DIR)/u-boot.bin.sd.bin $(OUT_DIR)/u-boot-$(UBOOT_TAG).bin
+$(OUT_DIR)/u-boot-$(UBOOT_TAG)-$(RAM_SIZE).bin: $(OUT_DIR) $(FIP_DIR)/u-boot.bin.sd.bin
+	cp $(FIP_DIR)/u-boot.bin.sd.bin $(OUT_DIR)/u-boot-$(UBOOT_TAG)-$(RAM_SIZE).bin
 
 u-boot:
 	git clone --branch $(UBOOT_TAG) --single-branch https://github.com/arnarg/u-boot.git $(UBOOT_DIR)
@@ -23,6 +25,9 @@ u-boot:
 $(UBOOT_OUTPUT_DIR)/u-boot.bin: u-boot
 	$(UBOOT_MAKE) $(UBOOT_DEFCONFIG)
 	$(UBOOT_MAKE) -j $$(nproc)
+
+$(CURDIR)/fip:
+	tar xvf files/gxl-p281-fip-$(RAM_SIZE).tar.gz -C $(CURDIR)
 
 $(OUT_DIR):
 	mkdir -p $(OUT_DIR)
@@ -32,7 +37,7 @@ $(TMP_DIR):
 
 .PHONY: clean superclean
 clean:
-	rm -rf out tmp
+	rm -rf out tmp fip
 
 superclean: clean
 	rm -rf u-boot
--- Makefile.fip.mk
+++ Makefile.fip.mk
@@ -1,17 +1,17 @@
-$(FIP_DIR)/bl30_new.bin.enc: $(FIP_DIR)/bl30_new.bin
+$(FIP_DIR)/bl30_new.bin.enc: $(CURDIR)/fip $(FIP_DIR)/bl30_new.bin
 	$(CURDIR)/fip/gxl/aml_encrypt_gxl --bl3enc --input $(FIP_DIR)/bl30_new.bin
 
-$(FIP_DIR)/bl31.img.enc: $(FIP_DIR)/bl31.img
+$(FIP_DIR)/bl31.img.enc: $(CURDIR)/fip $(FIP_DIR)/bl31.img
 	$(CURDIR)/fip/gxl/aml_encrypt_gxl --bl3enc --input $(FIP_DIR)/bl31.img
 
-$(FIP_DIR)/bl33.bin.enc: $(FIP_DIR)/bl33.bin
+$(FIP_DIR)/bl33.bin.enc: $(CURDIR)/fip $(FIP_DIR)/bl33.bin
 	$(CURDIR)/fip/gxl/aml_encrypt_gxl --bl3enc --input $(FIP_DIR)/bl33.bin
 
-$(FIP_DIR)/bl2.n.bin.sig: $(FIP_DIR)/bl2_new.bin
+$(FIP_DIR)/bl2.n.bin.sig: $(CURDIR)/fip $(FIP_DIR)/bl2_new.bin
 	$(CURDIR)/fip/gxl/aml_encrypt_gxl --bl2sig --input $(FIP_DIR)/bl2_new.bin \
 	--output $(FIP_DIR)/bl2.n.bin.sig
 
-$(FIP_DIR)/u-boot.bin $(FIP_DIR)/u-boot.bin.sd.bin: $(FIP_DIR)/bl30_new.bin.enc $(FIP_DIR)/bl31.img.enc $(FIP_DIR)/bl33.bin.enc $(FIP_DIR)/bl2.n.bin.sig
+$(FIP_DIR)/u-boot.bin $(FIP_DIR)/u-boot.bin.sd.bin: $(CURDIR)/fip $(FIP_DIR)/bl30_new.bin.enc $(FIP_DIR)/bl31.img.enc $(FIP_DIR)/bl33.bin.enc $(FIP_DIR)/bl2.n.bin.sig
 	$(CURDIR)/fip/gxl/aml_encrypt_gxl --bootmk \
 	--output $(FIP_DIR)/u-boot.bin \
 	--bl2 $(FIP_DIR)/bl2.n.bin.sig \
@@ -19,7 +19,7 @@ $(FIP_DIR)/u-boot.bin $(FIP_DIR)/u-boot.bin.sd.bin: $(FIP_DIR)/bl30_new.bin.enc 
 	--bl31 $(FIP_DIR)/bl31.img.enc \
 	--bl33 $(FIP_DIR)/bl33.bin.enc
 
-$(FIP_DIR)/bl2_new.bin: $(FIP_DIR)/bl2_acs.bin $(FIP_DIR)/bl21.bin
+$(FIP_DIR)/bl2_new.bin: $(CURDIR)/fip $(FIP_DIR)/bl2_acs.bin $(FIP_DIR)/bl21.bin
 	$(CURDIR)/fip/blx_fix.sh \
 	$(FIP_DIR)/bl2_acs.bin \
 	$(FIP_DIR)/zero_tmp \
@@ -29,14 +29,14 @@ $(FIP_DIR)/bl2_new.bin: $(FIP_DIR)/bl2_acs.bin $(FIP_DIR)/bl21.bin
 	$(FIP_DIR)/bl2_new.bin \
 	bl2
 
-$(FIP_DIR)/bl2_acs.bin: $(FIP_DIR)/bl2.bin $(FIP_DIR)/acs.bin
+$(FIP_DIR)/bl2_acs.bin: $(CURDIR)/fip $(FIP_DIR)/bl2.bin $(FIP_DIR)/acs.bin
 	python2 $(CURDIR)/fip/acs_tool.pyc \
 	$(FIP_DIR)/bl2.bin \
 	$(FIP_DIR)/bl2_acs.bin \
 	$(FIP_DIR)/acs.bin \
 	0
 
-$(FIP_DIR)/bl30_new.bin: $(FIP_DIR)/bl30.bin $(FIP_DIR)/bl301.bin
+$(FIP_DIR)/bl30_new.bin: $(CURDIR)/fip $(FIP_DIR)/bl30.bin $(FIP_DIR)/bl301.bin
 	$(CURDIR)/fip/blx_fix.sh \
 	$(FIP_DIR)/bl30.bin \
 	$(FIP_DIR)/zero_tmp \
@@ -48,17 +48,17 @@ $(FIP_DIR)/bl30_new.bin: $(FIP_DIR)/bl30.bin $(FIP_DIR)/bl301.bin
 
 $(FIP_DIR): $(UBOOT_OUTPUT_DIR)/u-boot.bin
 	mkdir -p $(FIP_DIR)
-$(FIP_DIR)/bl2.bin: $(FIP_DIR)
+$(FIP_DIR)/bl2.bin: $(CURDIR)/fip $(FIP_DIR)
 	cp $(CURDIR)/fip/gxl/bl2.bin $(FIP_DIR)/
-$(FIP_DIR)/acs.bin: $(FIP_DIR)
+$(FIP_DIR)/acs.bin: $(CURDIR)/fip $(FIP_DIR)
 	cp $(CURDIR)/fip/gxl/acs.bin $(FIP_DIR)/
-$(FIP_DIR)/bl21.bin: $(FIP_DIR)
+$(FIP_DIR)/bl21.bin: $(CURDIR)/fip $(FIP_DIR)
 	cp $(CURDIR)/fip/gxl/bl21.bin $(FIP_DIR)/
-$(FIP_DIR)/bl30.bin: $(FIP_DIR)
+$(FIP_DIR)/bl30.bin: $(CURDIR)/fip $(FIP_DIR)
 	cp $(CURDIR)/fip/gxl/bl30.bin $(FIP_DIR)/
-$(FIP_DIR)/bl301.bin: $(FIP_DIR)
+$(FIP_DIR)/bl301.bin: $(CURDIR)/fip $(FIP_DIR)
 	cp $(CURDIR)/fip/gxl/bl301.bin $(FIP_DIR)/
-$(FIP_DIR)/bl31.img: $(FIP_DIR)
+$(FIP_DIR)/bl31.img: $(CURDIR)/fip $(FIP_DIR)
 	cp $(CURDIR)/fip/gxl/bl31.img $(FIP_DIR)/
-$(FIP_DIR)/bl33.bin: $(UBOOT_OUTPUT_DIR)/u-boot.bin $(FIP_DIR)
+$(FIP_DIR)/bl33.bin: $(UBOOT_OUTPUT_DIR)/u-boot.bin $(CURDIR)/fip $(FIP_DIR)
 	cp $(UBOOT_OUTPUT_DIR)/u-boot.bin $(FIP_DIR)/bl33.bin
